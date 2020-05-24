35585405427cece2e3e1e696e2baeca39336321f
Carol Bras <carolina.bcosta1@gmail.com>
2020-05-24 02:53:32 -0300

    Add Layout
    First pages of template and style sheets

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <AppTopbar />
+
+    <div class="layout-main ">
+      <router-view />
+    </div>
+
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+
+import AppTopbar from './AppTopbar.vue';
+import AppFooter from './AppFooter.vue';
+
+export default {
+
+  components: {
+    AppTopbar,
+    AppFooter
+  }
+  
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="layout-footer">
+		<span class="footer-text" style="margin-left: 5px">@brascarol - 2020</span>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "AppFooter"
+	}
+</script>
