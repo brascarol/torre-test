b9b6dbc3ee78a8184ef5bed98d0e3fd1ea15cb35
Carol Bras <carolina.bcosta1@gmail.com>
2020-05-24 02:54:57 -0300

    Main Pages

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div style="text-align:center;height:100%">
+    <hr />
+    <div class="layout-topbar2">
+      <span class="p-input-icon-left">
+        <i class="pi pi-search" />
+        <InputText type="text" placeholder="Search People" @keyup.native.enter="search($event)" />
+      </span>
+    </div>
+    <div class="cards" v-for="r in results" :key="r.subjectId">
+      <Card class="card">
+        <template slot="header">
+          <div v-if="r.picture !== null">
+            <img class="img" :src="r.picture" />
+          </div>
+          <div v-else>
+            <img class="img" src="../assets/default.jpg" />
+          </div>
+        </template>
+        <template slot="title" style="font-size: 10px">{{r.name}}</template>
+        <template slot="content"></template>
+        <template slot="footer">
+          <Button icon="pi pi-check" label="View" @click="openBio(r.username)" />
+        </template>
+      </Card>
+    </div>
+  </div>
+</template>
+
+<script>
+import TorreService from "../services/TorreService";
+
+export default {
+  data() {
+    return {
+      results: []
+    };
+  },
+  methods: {
+    async search(event) {
+      this.results = await TorreService.postPeople({
+        name: { term: event.target.value },
+        limit: 10
+      }).catch(e => {
+        console.log(e);
+      });
+    },
+    openBio(username){
+     this.bio = TorreService.getBio(username);
+     this.$router.replace({name:'home', params:{}});
+
+
+    }
+  }
+};
+</script>
+
